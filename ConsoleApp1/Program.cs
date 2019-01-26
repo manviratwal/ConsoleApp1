d2be760ae7e9db658073984531d2117722b9b895
visualstudio <visualstudio@122-PC33>
2019-01-25 21:18:47 -0500

    Second commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,11 +6,49 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-    class Program
+   
+class Program
     {
         static void Main(string[] args)
         {
- 
         }
     }
+    class Countrysider
+    {
+
+        Village Alst;
+        Village Maeland;
+        Village Schenig;
+
+        public void InitializeMap()
+        {
+
+
+            Alst = new Village();
+            Maeland = new Village();
+            Maeland.isAstrildeHere = true;
+            Schenig = new Village();
+
+            Alst.NextVillage = Maeland;
+            Maeland.NextVillage = Schenig;
+            Schenig.NextVillage = Alst;
+        }
+
+        public void WalkAround()
+        {
+            Village InitializeVillage = this.Maeland;
+        }
+    }
+
+    class Village
+    {
+        public Village PreviousVillage;
+        public Village NextVillage;
+        public String VillageName;
+        public bool isAstrildeHere = false;
+        public Village() { }
+
+
+    }
 }
+
