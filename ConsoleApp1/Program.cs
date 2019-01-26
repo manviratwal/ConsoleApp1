bb01231516f970af72faa0f81c9e8bb12ed7bf29
visualstudio <visualstudio@122-PC33>
2019-01-25 21:29:35 -0500

    third commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,50 +5,60 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace ConsoleApp1
-{
-   
+{ 
 class Program
+{
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-        }
     }
-    class Countrysider
+}
+class Countrysider
+{
+
+    Village Alst;
+    Village Maeland;
+    Village Schenig;
+
+    public void InitializeMap()
     {
 
-        Village Alst;
-        Village Maeland;
-        Village Schenig;
 
-        public void InitializeMap()
-        {
+        Alst = new Village();
+        Alst.VillageName = "Alst";
+        Maeland = new Village();
+        Maeland.VillageName = "Maeland";
+        Maeland.isAstrildeHere = true;
+        Schenig = new Village();
+        Schenig.VillageName = "Schenig";
 
-
-            Alst = new Village();
-            Maeland = new Village();
-            Maeland.isAstrildeHere = true;
-            Schenig = new Village();
-
-            Alst.NextVillage = Maeland;
-            Maeland.NextVillage = Schenig;
-            Schenig.NextVillage = Alst;
-        }
-
-        public void WalkAround()
-        {
-            Village InitializeVillage = this.Maeland;
-        }
+        Alst.NextVillage = Maeland;
+        Maeland.NextVillage = Schenig;
+        Schenig.NextVillage = Alst;
     }
 
-    class Village
+    public void FindAstrilde()
     {
+
+        Village CurrentVillage = this.Maeland;
+        Village NextVillage;
+        if (CurrentVillage.isAstrildeHere)
+        {
+            Console.WriteLine("Hugi Found Astrilde!!! Celebration!! XoXo!!");
+        }
+
+        NextVillage = CurrentVillage.NextVillage;
+    }
+}
+
+class Village
+{
         public Village PreviousVillage;
         public Village NextVillage;
-        public String VillageName;
-        public bool isAstrildeHere = false;
-        public Village() { }
+    public String VillageName;
+    public bool isAstrildeHere = false;
+    public Village() { }
 
 
-    }
+}
 }
 
